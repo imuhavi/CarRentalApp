d8fd15801b0b1a23b2c0bc506983f115073c5451
Robbe Keters <robbeketers@gmail.com>
2018-10-30 11:10:31 +0100

    idfsg

--- CarRentalApp/src/session/IManagerSession.java
+++ CarRentalApp/src/session/IManagerSession.java
@@ -1,23 +1,24 @@
 package session;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 import rental.ICarRentalCompany;
 
 public interface IManagerSession extends Remote {
 
-	public void registerCRC(ICarRentalCompany crc);
+	public void registerCRC(ICarRentalCompany crc, String name) throws RemoteException;
 	
-	public void unregisterCRC(String name);
+	public void unregisterCRC(String name) throws RemoteException;
 	
-	public ICarRentalCompany getCRC(String name);
+	public ICarRentalCompany getCRC(String name) throws RemoteException;
 	
-	public List<String> getCarTypes(ICarRentalCompany crc);
+	public List<String> getCarTypes(String crc) throws RemoteException;
 	
-	public int getNbReservationCarType(ICarRentalCompany crc);
+	public int getNbReservationCarType(String crc) throws RemoteException;
 	
-	public String getBestCustomer();
+	public String getBestCustomer() throws RemoteException;
 	
-	public String getMostPopularCarType(int year);
+	public String getMostPopularCarType(int year) throws RemoteException;
 }
